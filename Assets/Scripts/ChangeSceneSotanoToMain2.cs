2eb66676abd88c44b86f3bd4a273402353767a90
EmaBilibili <emabilibili@gmail.com>
2023-10-30 00:56:58 -0300

    Fix mouse Scenes

---
+++ Assets/Scripts/ChangeSceneSotanoToMain2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneSotanoToMain2 : MonoBehaviour
+{
+    public string nombreDeEscena; // Nombre de la escena a la que deseas cambiar
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Cambia "Player" al tag de tu objeto si es diferente
+        {
+            SceneManager.LoadScene(nombreDeEscena);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
